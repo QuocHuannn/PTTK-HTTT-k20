d20aeabba85ca703a1c5e90a298b12b914dd41b2
Huynhminhduc2k2 <hmduc20@clc.fitus.edu.vn>
2023-04-27 17:52:48 +0700

    Revert "Layout for MainUI"
    This reverts commit 5894dd9a4bb60c31abb27b8ea300c8cb91175a47.

--- PTTKHTTT/Program.cs
+++ PTTKHTTT/Program.cs
@@ -11,8 +11,7 @@ namespace PTTKHTTT
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new iSignUp());
-            Application.Run(new iMainUI());
+            Application.Run(new iSignUp());
         }
     }
 }
