1a251ffd8bca048cfae0cc1fe9623b7e5aec1935
Luss <nhok8rytaband@gmail.com>
2023-05-10 17:14:05 +0700

    Thay đổi TT

--- Source/App/PTTKHTTT/ThayDoiTT.Designer.cs
+++ Source/App/PTTKHTTT/ThayDoiTT.Designer.cs
@@ -38,6 +38,10 @@
             this.txtSdt = new System.Windows.Forms.TextBox();
             this.Confirm = new System.Windows.Forms.Button();
             this.Huy = new System.Windows.Forms.Button();
+            this.Gender = new System.Windows.Forms.Label();
+            this.Address = new System.Windows.Forms.Label();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -63,7 +67,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(36, 132);
+            this.label2.Location = new System.Drawing.Point(36, 115);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(131, 21);
             this.label2.TabIndex = 2;
@@ -71,7 +75,7 @@
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(220, 130);
+            this.txtName.Location = new System.Drawing.Point(220, 113);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(357, 23);
             this.txtName.TabIndex = 18;
@@ -80,7 +84,7 @@
             // 
             this.CMND.AutoSize = true;
             this.CMND.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.CMND.Location = new System.Drawing.Point(36, 192);
+            this.CMND.Location = new System.Drawing.Point(36, 158);
             this.CMND.Name = "CMND";
             this.CMND.Size = new System.Drawing.Size(60, 21);
             this.CMND.TabIndex = 19;
@@ -88,7 +92,7 @@
             // 
             // txtCMND
             // 
-            this.txtCMND.Location = new System.Drawing.Point(220, 190);
+            this.txtCMND.Location = new System.Drawing.Point(220, 160);
             this.txtCMND.Name = "txtCMND";
             this.txtCMND.Size = new System.Drawing.Size(357, 23);
             this.txtCMND.TabIndex = 20;
@@ -97,7 +101,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(36, 243);
+            this.label4.Location = new System.Drawing.Point(36, 215);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(111, 21);
             this.label4.TabIndex = 21;
@@ -105,34 +109,74 @@
             // 
             // txtSdt
             // 
-            this.txtSdt.Location = new System.Drawing.Point(220, 241);
+            this.txtSdt.Location = new System.Drawing.Point(220, 213);
             this.txtSdt.Name = "txtSdt";
             this.txtSdt.Size = new System.Drawing.Size(357, 23);
             this.txtSdt.TabIndex = 22;
+            this.txtSdt.TextChanged += new System.EventHandler(this.txtSdt_TextChanged);
             // 
             // Confirm
             // 
-            this.Confirm.Location = new System.Drawing.Point(631, 357);
+            this.Confirm.Location = new System.Drawing.Point(634, 370);
             this.Confirm.Name = "Confirm";
             this.Confirm.Size = new System.Drawing.Size(83, 41);
             this.Confirm.TabIndex = 23;
             this.Confirm.Text = "Xác nhận";
             this.Confirm.UseVisualStyleBackColor = true;
+            this.Confirm.Click += new System.EventHandler(this.Confirm_Click);
             // 
             // Huy
             // 
-            this.Huy.Location = new System.Drawing.Point(36, 357);
+            this.Huy.Location = new System.Drawing.Point(36, 370);
             this.Huy.Name = "Huy";
             this.Huy.Size = new System.Drawing.Size(83, 41);
             this.Huy.TabIndex = 24;
             this.Huy.Text = "Hủy";
             this.Huy.UseVisualStyleBackColor = true;
             // 
+            // Gender
+            // 
+            this.Gender.AutoSize = true;
+            this.Gender.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Gender.Location = new System.Drawing.Point(36, 265);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(76, 21);
+            this.Gender.TabIndex = 25;
+            this.Gender.Text = "Giới tính";
+            // 
+            // Address
+            // 
+            this.Address.AutoSize = true;
+            this.Address.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Address.Location = new System.Drawing.Point(36, 310);
+            this.Address.Name = "Address";
+            this.Address.Size = new System.Drawing.Size(63, 21);
+            this.Address.TabIndex = 26;
+            this.Address.Text = "Địa chỉ";
+            // 
+            // txtGender
+            // 
+            this.txtGender.Location = new System.Drawing.Point(220, 263);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(357, 23);
+            this.txtGender.TabIndex = 27;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(220, 308);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(357, 23);
+            this.txtAddress.TabIndex = 28;
+            // 
             // ThayDoiTT
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.Address);
+            this.Controls.Add(this.Gender);
             this.Controls.Add(this.Huy);
             this.Controls.Add(this.Confirm);
             this.Controls.Add(this.txtSdt);
@@ -163,5 +207,9 @@
         private TextBox txtSdt;
         private Button Confirm;
         private Button Huy;
+        private Label Gender;
+        private Label Address;
+        private TextBox txtGender;
+        private TextBox txtAddress;
     }
 }
